bb12e16d6beca016f9aed7a3d5ff1686b20757f1
thomasschiwietz <tschiwietz@lyft.com>
2017-12-28 13:16:16 +0100

    linearize depth

--- sdl_viewer/shaders/quad_drawer.fs
+++ sdl_viewer/shaders/quad_drawer.fs
@@ -7,9 +7,15 @@ uniform sampler2D aTex;
 
 layout(location = 0) out vec4 FragColor;
 
+float linearize_depth(float depth)
+{
+    float zNear = 0.1;
+    float zFar  = 75.0;
+    return (2.0 * zNear) / (zFar + zNear - depth * (zFar - zNear));
+}
+
 void main()
 {
-	float d = texture(aTex, tex.xy).x;
-	d = clamp(d - 0.99, 0.0, 1.0) * 100.0;
-	FragColor = vec4(d);
+    float d = texture(aTex, tex.xy).x;
+    FragColor = d < 1.0 ? vec4(1.0 - linearize_depth(d)) : vec4(0.3,0.3,1.0,1.0);   // infinite depth blue sky
 }
