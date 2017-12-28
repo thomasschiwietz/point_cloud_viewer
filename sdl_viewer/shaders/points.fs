11d0f548ab319068e01f59165d98b9aa9917d39b
thomasschiwietz <tschiwietz@lyft.com>
2017-12-28 10:13:53 +0100

    add output layout pos

--- sdl_viewer/shaders/points.fs
+++ sdl_viewer/shaders/points.fs
@@ -1,9 +1,13 @@
-#version 100 
+#version 330 core
+
+// inputs
+in vec4 v_color;
+
+// outputs
+layout(location = 0) out vec4 FragColor;
 
 precision mediump float;
 
-varying vec4 v_color;
-
 void main() {
- gl_FragColor = v_color;
+    FragColor = v_color;
 }
