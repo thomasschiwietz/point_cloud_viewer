1195ac98566bf961874723f904da403fbb9fcfd7
thomasschiwietz <tschiwietz@lyft.com>
2018-01-09 10:07:39 +0100

    replace tabs with spaces

--- sdl_viewer/shaders/filledBox.fs
+++ sdl_viewer/shaders/filledBox.fs
@@ -8,7 +8,7 @@ layout(location = 0) out vec4 FragColor;
 
 void main()
 {
-	vec3 lightDir = vec3(0,0,1);
-	float diff = max(dot(normal, lightDir), 0.0);
-	FragColor = diff * color;
+    vec3 lightDir = vec3(0,0,1);
+    float diff = max(dot(normal, lightDir), 0.0);
+    FragColor = diff * color;
 }
